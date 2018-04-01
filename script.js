23492ba61888317db9d035b8a2aa9f31fd597a83
pwalsh1439 <32913692+pwalsh1439@users.noreply.github.com>
2018-04-01 20:51:47 +0100

    s

--- script.js
+++ script.js
@@ -103,21 +103,20 @@ function loadMap(){
 	//create popup. add listner to featurre group that runs functions when clicked 
 	markersLayer.on('click', markerOnClick);
 	
-	// ** /*
+	/*
 	//set up new geocoder object. add listner to post code search that runs function
 	var geocoder = new google.maps.Geocoder();
     document.getElementById('postcode_search').addEventListener('click', function() {
 		postcodeSearch(geocoder, ofstedMap);
     });
-	//** */
+	*/
 	// Add listner to post code search that runs postcode geocodeer function
-	//document.getElementById('postcode_search').addEventListener('click', function() {
-	//	postcodeSearch(ofstedMap);
-    //});
+	document.getElementById('postcode_search').addEventListener('click', function() {
+		postcodeSearch(ofstedMap);
+    });
 
 //function to perform popup process.
 //modify the side bar with the marker property information.
-/*
 function markerOnClick(e) {
 	var attributes = e.layer.properties;
 	document.getElementById('updatehtml').innerHTML = 
@@ -133,5 +132,5 @@ function markerOnClick(e) {
 			"<b>Deprivation:</b> " + attributes.Deprivatio + "<br>" +
 			"<b>As at Date:</b> " + attributes.As_at_date + "<br>"
 			};
+	
 	};
-*/
